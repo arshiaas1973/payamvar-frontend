3014d7a87f56b98865786f6779d2ac872ce35359
Arshia Esmailzadeh <arshiaas1973@gmail.com>
2025-12-30 23:54:30 +0330

    feat: update ChatHeader and ChatProfile components with new button UI and last seen display

--- web/components/web/ChatHeader/client.tsx
+++ web/components/web/ChatHeader/client.tsx
@@ -4,6 +4,8 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { create } from "zustand";
 import ChatProfile from "../ChatProfile";
+import { Button } from "@/components/ui/button";
+import LazyIcon from "@/components/ui/custom/lazyicon";
 
 const useStore = create<{
     isProfileShown: boolean,
@@ -41,14 +43,16 @@ export function ChatHeaderClient() {
                 </DialogContent>
             </Dialog>
             <div className="flex flex-row items-center gap-2">
-                <Avatar className="w-10 h-10">
-                    <AvatarImage width={40} height={40} />
-                    <AvatarFallback>
-                        <button className="cursor-pointer w-10 aspect-square rounded-full" type="button">
-                            AE
-                        </button>
-                    </AvatarFallback>
-                </Avatar>
+                <div className="flex flex-row-reverse items-center h-full relative cursor-pointer bg-colored-secondary/45 text-white rounded-full p-1 gap-1.5">
+                    <Button className="rounded-full outline-none! border-0! bg-secondary-background/50! peer" variant="outline">
+                        <LazyIcon name="line-md:list" className="size-full aspect-square" />
+                    </Button>
+                    <div className="w-0 origin-right scale-x-0 h-full rounded-full bg-colored-secondary/60 flex flex-row peer-hover:w-20! peer-hover:scale-x-100! transition-all duration-300 ease-linear">
+                        <Button className="rounded-full outline-none! border-0! bg-secondary-background/90!" variant="outline">
+                            <LazyIcon name="line-md:list" className="size-full aspect-square" />
+                        </Button>
+                    </div>
+                </div>
             </div>
         </div>
     )
